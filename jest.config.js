dcd5812ef8ec8ea8e6d428db14a1a033c7fcbd6d
Aleksanteri Heinonen <fsalhe@linux-desktop11.tuni.fi>
2025-11-19 13:49:10 +0200

    line fix, convert to esm module

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,12 @@
-module.exports = {
-  testEnvironment: "node",
-  transform: {
-    "^.+\\.js$": "babel-jest"
-  },
-  transformIgnorePatterns: ["/node_modules/(?!assignment-lib)"],
-  moduleFileExtensions: ["js", "mjs"],
-  testPathIgnorePatterns: ["/.internal/"],
-  coveragePathIgnorePatterns: ["/.internal/"], 
-  testMatch: ["**/tests/**/*.test.js"],
-  collectCoverage: true,
-  collectCoverageFrom: ["node_modules/assignment-lib/src/**/*.js"],
-  coverageReporters: ["lcov", "text"]
+export const testEnvironment = "node";
+export const transform = {
+  "^.+\\.js$": "babel-jest"
 };
+export const transformIgnorePatterns = ["/node_modules/(?!assignment-lib)"];
+export const moduleFileExtensions = ["js", "mjs"];
+export const testPathIgnorePatterns = ["/.internal/"];
+export const coveragePathIgnorePatterns = ["/.internal/"];
+export const testMatch = ["**/tests/**/*.test.js"];
+export const collectCoverage = true;
+export const collectCoverageFrom = ["node_modules/assignment-lib/src/**/*.js"];
+export const coverageReporters = ["lcov", "text"];
