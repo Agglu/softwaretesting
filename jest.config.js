13a93eab5a3a1bd8b3c675640c16f70f56ff0043
Aleksanteri Heinonen <fsalhe@linux-desktop11.tuni.fi>
2025-11-19 13:45:48 +0200

    try with different setup

--- jest.config.js
+++ jest.config.js
@@ -1,23 +1,14 @@
-export default {
-    testEnvironment: "node",
-    transform: {
-        "^.+\\.js$": "babel-jest"
-      },
-    moduleNameMapper: {
-      "^assignment-lib/(.*)$": "<rootDir>/node_modules/assignment-lib/src/$1"
-    },      
-    transformIgnorePatterns: [
-        "/node_modules/(?!assignment-lib)"
-    ],
-    moduleFileExtensions: ["js", "mjs"],
-    testPathIgnorePatterns: ["/node_modules/", "/.internal/"],
-    coveragePathIgnorePatterns: ["/node_modules/", "/.internal/"],
-    testMatch: ["**/tests/**/*.test.js"],
-    collectCoverage: true,
-    collectCoverageFrom: [
-      "<rootDir>/node_modules/assignment-lib/src/**/*.js",
-      "!<rootDir>/node_modules/assignment-lib/src/.internal/**"
-    ],
-    coverageReporters: ["lcov", "text"],
-  };
-  
+module.exports = {
+  testEnvironment: "node",
+  transform: {
+    "^.+\\.js$": "babel-jest"
+  },
+  transformIgnorePatterns: ["/node_modules/(?!assignment-lib)"],
+  moduleFileExtensions: ["js", "mjs"],
+  testPathIgnorePatterns: ["/.internal/"],
+  coveragePathIgnorePatterns: ["/.internal/"], 
+  testMatch: ["**/tests/**/*.test.js"],
+  collectCoverage: true,
+  collectCoverageFrom: ["node_modules/assignment-lib/src/**/*.js"],
+  coverageReporters: ["lcov", "text"]
+};
