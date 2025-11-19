ca43a6ef0b872fc79e66a6e80ef1edd4335bc575
Aleksanteri Heinonen <fsalhe@linux-desktop9.tuni.fi>
2025-11-19 15:31:24 +0200

    add singular tests

--- tests/filter.test.js
+++ tests/filter.test.js
@@ -1,5 +1,13 @@
 import filter from "../src/filter.js";
 
 describe("filter", () => {
+    test("should return X", () => {
+        const users = [
+            { 'user': 'matti', 'active': true },
+            { 'user': 'pekka', 'active': false }
+          ];
 
+        const result = filter(users, ({ active }) => active);
+        expect(result).toEqual([{"active": true, "user": "matti"}]);
+      });
 });
--- tests/isDate.test.js
+++ tests/isDate.test.js
@@ -1,5 +1,7 @@
 import isDate from "../src/isDate.js";
 
 describe("isDate", () => {
-
+    test('returns false for objects that are not empty', () => {
+        expect(isDate(new Date)).to.equal(true)
+    });
 });
