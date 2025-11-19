f7b1ec8e4c48b1ff3b5671885831cdf975ab5899
Aleksanteri Heinonen <fsalhe@linux-desktop11.tuni.fi>
2025-11-19 13:53:07 +0200

    use cjs instead of js

---
+++ jest.config.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+  testEnvironment: "node",
+  transform: {
+    "^.+\\.js$": "babel-jest"
+  },
+  transformIgnorePatterns: ["/node_modules/(?!assignment-lib)"],
+  moduleFileExtensions: ["js", "mjs"],
+  testPathIgnorePatterns: ["/.internal/"],
+  coveragePathIgnorePatterns: ["/.internal/"],
+  testMatch: ["**/tests/**/*.test.js"],
+  collectCoverage: true,
+  collectCoverageFrom: ["node_modules/assignment-lib/src/**/*.js"],
+  coverageReporters: ["lcov", "text"]
+};
