28cdc869b4b517e724eada83c788e364585509d6
Aleksanteri Heinonen <fsalhe@linux-desktop11.tuni.fi>
2025-11-19 13:14:09 +0200

    add jest config file

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+export default {
+    testEnvironment: "node",
+    transform: {
+        "^.+\\.js$": "babel-jest"
+      },
+    transformIgnorePatterns: [
+        "/node_modules/(?!assignment-lib)"
+    ],
+    moduleFileExtensions: ["js", "mjs"],
+    testPathIgnorePatterns: ["/node_modules/", "/.internal/"],
+    coveragePathIgnorePatterns: ["/node_modules/", "/.internal/"],
+    collectCoverage: true,
+    coverageReporters: ["lcov", "text"],
+  };
+  
