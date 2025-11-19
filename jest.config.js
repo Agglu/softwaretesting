285acce95be5a19dcf1bc78418095ee66dba1de2
Aleksanteri Heinonen <fsalhe@linux-desktop11.tuni.fi>
2025-11-19 13:35:10 +0200

    collect coverage from source library functions

--- jest.config.js
+++ jest.config.js
@@ -9,10 +9,10 @@ export default {
     moduleFileExtensions: ["js", "mjs"],
     testPathIgnorePatterns: ["/node_modules/", "/.internal/"],
     coveragePathIgnorePatterns: ["/node_modules/", "/.internal/"],
+    testMatch: ["**/tests/**/*.test.js"],
     collectCoverage: true,
     collectCoverageFrom: [
-      "tests/*.js",
-      "!**/.internal/**"
+      "node_modules/assignment-lib/src/**/*.js"
     ],
     coverageReporters: ["lcov", "text"],
   };
