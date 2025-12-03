d14e8a0b91a92a55a442bcaef1fc5f631e2f52af
Aleksanteri Heinonen <fsalhe@linux-desktop10.tuni.fi>
2025-12-03 16:20:25 +0200

    change syntax to Jest style

--- tests/isDate.test.js
+++ tests/isDate.test.js
@@ -2,10 +2,10 @@ import isDate from "../src/isDate.js";
 
 describe("isDate", () => {
   test("returns true for valid date objects", () => {
-    expect(isDate(new Date())).to.equal(true);
+    expect(isDate(new Date())).toBe(true);
   });
 
   test("returns false for invalid date objects", () => {
-    expect(isDate("Mon April 23 2012")).to.equal(false);
+    expect(isDate("Mon April 23 2012")).toBe(false);
   });
 });
