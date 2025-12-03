ff79a7e441f9e375e365653582cd88ba8b5041ab
Aleksanteri Heinonen <fsalhe@linux-desktop12.tuni.fi>
2025-12-03 13:27:37 +0200

    add tests for capitalize function

--- tests/capitalize.test.js
+++ tests/capitalize.test.js
@@ -1,7 +1,55 @@
 import capitalize from "../src/capitalize.js";
 
 describe("capitalize", () => {
+    test('empty string', () => {
+      expect(capitalize('')).toBe('');
+    });
+
     test('string with lowercase letters', () => {
-      expect(capitalize('fred')).toBe('Fred');
+      expect(capitalize('foo')).toBe('Foo');
+    });
+
+    test('string with uppercase letters', () => {
+      expect(capitalize('FOO')).toBe('Foo');
+    });
+
+    test('capitalized string', () => {
+      expect(capitalize('Bar')).toBe('Bar');
+    });
+
+    test('string with uppercase characters', () => {
+      expect(capitalize('bAr')).toBe('Bar');
+    });
+
+    test('string that starts with numbers', () => {
+      expect(capitalize('1STRING')).toBe('1string');
+    });
+
+    test('string that has numbers in it', () => {
+      expect(capitalize('STRING1')).toBe('String1');
+    });
+
+    test('string with whitespaces', () => {
+      expect(capitalize('FOO BAR')).toBe('Foo bar');
+    });
+
+    test('number', () => {
+      expect(capitalize(123)).toThrow();
+    });
+
+    test('array', () => {
+      expect(capitalize([foo, bar])).toThrow();
+    });
+
+    test('object', () => {
+      expect(capitalize({'foo': 'bar'})).toThrow();
+    });
+
+    test('undefined', () => {
+      expect(capitalize(undefined)).toThrow();
+    });
+
+    test('null', () => {
+      expect(capitalize(null)).toThrow();
     });
 });
