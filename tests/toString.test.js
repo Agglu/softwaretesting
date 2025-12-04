4e3f9992f1f96cc4594bae9c9d891ba3c8b48352
Aleksanteri Heinonen <fsalhe@linux-desktop9.tuni.fi>
2025-12-04 13:16:35 +0200

    add toString tests

--- tests/toString.test.js
+++ tests/toString.test.js
@@ -1,16 +1,50 @@
 import toString from "../src/toString.js";
 
-describe("toString", () => {
-  test("undefined and null to an empty string", () => {
-    expect(toString(undefined)).toBe("");
-    expect(toString(null)).toBe("");
+describe('toString', () => {
+  test('undefined and null to an empty string', () => {
+    expect(toString(undefined)).toBe('');
+    expect(toString(null)).toBe('');
   });
 
-  test("[1, 2, 3] to '1,2,3'", () => {
-    expect(toString([1, 2, 3])).toBe("1,2,3");
+  test('NaN', () => {
+    expect(toString(NaN)).toBe('NaN');
   });
 
-  test("Negative integers to string", () => {
-    expect(toString(-1)).toBe("-1");
+  // Boolean values
+  test('boolean', () => {
+    expect(toString(true)).toBe('true');
+    expect(toString(false)).toBe('false');
+  });
+
+  test('integer', () => {
+    expect(toString(500)).toBe('500');
+  });
+
+  test('negative integer', () => {
+    expect(toString(-1)).toBe('-1');
+  });
+
+  test('float', () => {
+    expect(toString(6.7)).toBe('6.7');
+  });
+
+  test('string', () => {
+    expect(toString('string')).toBe('string');
+    expect(toString('')).toBe('');
+    // emoji
+    expect(toString('😄')).toBe('😄');
+  });
+
+  test('array', () => {
+    expect(toString([1, 2, 3])).toBe('1,2,3');
+    expect(toString([])).toBe('');
+  });
+
+  test('object', () => {
+    expect({'hello': 'world'}).toBe('[object Object]');
+  });
+
+  test('symbol', () => {
+    expect(toString(Symbol('foo'))).toBe('Symbol(foo)');
   });
 });
