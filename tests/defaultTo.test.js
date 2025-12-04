fafa7d97f3ba153cd08d4590ae068ee5da636bcf
Aleksanteri Heinonen <fsalhe@linux-desktop9.tuni.fi>
2025-12-04 12:51:12 +0200

    add defaultTo tests

--- tests/defaultTo.test.js
+++ tests/defaultTo.test.js
@@ -1,11 +1,34 @@
 import defaultTo from "../src/defaultTo.js";
 
-describe("defaultTo", () => {
-  test("returns the default value when the input is null", () => {
+describe('defaultTo', () => {
+  // value is NOT NaN, null or undefined
+  test('return default value when the input is not null', () => {
+    expect(defaultTo(1, 10)).toBe(1);
+  });
+
+  test('return default value when the input is not null', () => {
+    expect(defaultTo('foo', 'bar')).toBe('foo');
+  });
+
+  test('return default value when the input is not null', () => {
+    expect(defaultTo([1, 2, 3], [4, 5, 6])).toBe([1, 2, 3]);
+  });
+
+  // value is NaN, null or undefined
+  test('returns the default value when the input is NaN', () => {
+    expect(defaultTo(NaN, 'foo')).toBe('foo');
+  });
+
+  test('returns the default value when the input is null', () => {
     expect(defaultTo(null, 10)).toBe(10);
   });
 
-  test("return default value when the input is not null", () => {
-    expect(defaultTo(1, 10)).toBe(1);
+  test('returns the default value when the input is undefined', () => {
+    expect(defaultTo(undefined, [1, 2, 3])).toBe([1, 2, 3]);
+  });
+
+  // both are null
+  test('returns null', () => {
+    expect(defaultTo(null, null)).toBe(null);
   });
 });
